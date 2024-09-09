c13b93a6f19b5add184d8b6f723bf1d45f4499f3
Alejandro Osornio <aoxo.contact@gmail.com>
2024-09-09 04:15:34 +0000

    firewall-common: Add FirewallRule

--- firewall-common/src/lib.rs
+++ firewall-common/src/lib.rs
@@ -2,6 +2,26 @@
 
 #[derive(Debug, Clone, Copy)]
 pub enum FirewallEvent {
-    Blocked([u8; 4]),
+    Blocked(core::net::SocketAddr),
     Pass,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum FirewallAction {
+    Accept,
+    Pass,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum FirewallMatch {
+    Mask(core::net::IpAddr),
+    Lit(core::net::IpAddr),
+    Socket(core::net::SocketAddr),
+    Port(u16),
+}
+
+#[derive(Debug, Clone, Copy)]
+pub struct FirewallRule {
+    action: FirewallAction,
+    matches: FirewallMatch,
 }
