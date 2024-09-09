53f1188ab92bcc9b6d26812c5d3b4ea17e3691cb
Alejandro Osornio <aoxo.contact@gmail.com>
2024-09-09 03:47:00 +0000

    firewall-ebpf: Create FIREWALL_EVENTS

--- firewall-ebpf/src/main.rs
+++ firewall-ebpf/src/main.rs
@@ -1,8 +1,17 @@
 #![no_std]
 #![no_main]
 
-use aya_ebpf::{bindings::xdp_action, macros::xdp, programs::XdpContext};
-use aya_log_ebpf::info;
+use aya_ebpf::{
+    bindings::xdp_action,
+    macros::{map, xdp},
+    maps::RingBuf,
+    programs::XdpContext,
+};
+use aya_log_ebpf::error;
+use firewall_common::FirewallEvent;
+
+#[map]
+static FIREWALL_EVENTS: RingBuf = RingBuf::with_byte_size(4096, 0);
 
 #[xdp]
 pub fn firewall(ctx: XdpContext) -> u32 {
@@ -13,7 +22,14 @@ pub fn firewall(ctx: XdpContext) -> u32 {
 }
 
 fn try_firewall(ctx: XdpContext) -> Result<u32, u32> {
-    info!(&ctx, "received a packet");
+    if let Some(mut entry) = FIREWALL_EVENTS.reserve::<FirewallEvent>(0) {
+        unsafe { core::ptr::write_unaligned(entry.as_mut_ptr(), FirewallEvent::Pass) };
+
+        entry.submit(0);
+    } else {
+        error!(&ctx, "Failed to reserve entry for FirewallEvent")
+    }
+
     Ok(xdp_action::XDP_PASS)
 }
 
