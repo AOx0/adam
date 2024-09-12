f1ed8794ee36cfc1c03899533773cbb039cf0a5d
Alejandro Osornio <aoxo.contact@gmail.com>
2024-09-12 10:14:52 -0600

    controller: Use message in controller

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,12 +1,17 @@
 use std::{io::Write, os::unix::net::UnixStream};
 
+use message::{bincode, Message};
+
 #[tokio::main]
 async fn main() {
     let mut stream = UnixStream::connect("/run/adam/firewall").unwrap();
 
-    for i in 0..500 {
-        stream.write_all(format!("Hi {i}\n").as_bytes()).unwrap();
-    }
+    // for i in 0..500 {
+    //     stream.write_all(format!("Hi {i}\n").as_bytes()).unwrap();
+    // }
 
-    stream.write_all(b"exit").unwrap();
+    let mut buf = Vec::new();
+
+    bincode::serialize_into(&mut buf, &Message::Terminate).unwrap();
+    stream.write_all(&buf).unwrap();
 }
