603190dc241aaad15f8b6f132761d7b72308ceea
Alejandro Osornio <aoxo.contact@gmail.com>
2024-09-16 21:38:37 -0600

    controller: Usea deadpool to implement a UDS Pool

--- controller/src/firewall/mod.rs
+++ controller/src/firewall/mod.rs
@@ -2,6 +2,42 @@ use super::*;
 use message::{bincode, Message};
 use std::{io::Write, os::unix::net::UnixStream};
 
+#[derive(Debug, Clone)]
+pub struct Manager;
+
+impl Manager {
+    pub fn new(size: usize) -> Pool<Manager> {
+        Pool::builder(Manager)
+            .max_size(size)
+            .build()
+            .expect("Failed to create UDS Pool")
+    }
+}
+
+impl deadpool::managed::Manager for Manager {
+    type Type = Socket;
+    type Error = ();
+
+    async fn create(&self) -> Result<Self::Type, Self::Error> {
+        Ok(Socket::new())
+    }
+
+    async fn recycle(
+        &self,
+        _obj: &mut Self::Type,
+        _metrics: &deadpool::managed::Metrics,
+    ) -> deadpool::managed::RecycleResult<Self::Error> {
+        Ok(())
+    }
+}
+
+pub fn router() -> Router<AppState> {
+    Router::new()
+        .route("/start", post(start))
+        .route("/stop", post(stop))
+        .route("/halt", post(halt))
+}
+
 #[derive(Debug)]
 pub struct Socket {
     buf: Vec<u8>,
@@ -9,21 +45,21 @@ pub struct Socket {
 }
 
 pub async fn start(State(s): State<AppState>) {
-    s.firewall.lock().await.start()
+    s.firewall_pool.get().await.unwrap().start();
 }
 
 pub async fn stop(State(s): State<AppState>) {
-    s.firewall.lock().await.term()
+    s.firewall_pool.get().await.unwrap().term();
 }
 
 pub async fn halt(State(s): State<AppState>) {
-    s.firewall.lock().await.halt()
+    s.firewall_pool.get().await.unwrap().halt();
 }
 
 impl Socket {
     pub fn new() -> Self {
         Self {
-            buf: Vec::with_capacity(4096),
+            buf: Vec::with_capacity(512),
             stream: UnixStream::connect("/run/adam/firewall").unwrap(),
         }
     }
--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,19 +1,20 @@
-use std::sync::Arc;
-
 pub use axum::{extract::State, routing::post, Router};
-use tokio::{net::TcpListener, sync::Mutex};
+use deadpool::managed::Pool;
+use tokio::net::TcpListener;
 
 mod firewall;
 
+const POOL_SIZE: usize = 100;
+
 #[derive(Debug, Clone)]
 struct AppState {
-    firewall: Arc<Mutex<firewall::Socket>>,
+    firewall_pool: Pool<firewall::Manager>,
 }
 
 impl AppState {
     pub fn new() -> Self {
         Self {
-            firewall: Arc::new(Mutex::new(firewall::Socket::new())),
+            firewall_pool: firewall::Manager::new(POOL_SIZE),
         }
     }
 }
@@ -22,13 +23,9 @@ impl AppState {
 async fn main() {
     let state = AppState::new();
 
-    let firewall = Router::new()
-        .route("/start", post(firewall::start))
-        .route("/stop", post(firewall::stop))
-        .route("/halt", post(firewall::halt))
+    let router = Router::new()
+        .nest("/firewall", firewall::router())
         .with_state(state);
-
-    let router = Router::new().nest("/firewall", firewall);
 
     let listener = TcpListener::bind("[::]:80").await.unwrap();
     axum::serve(listener, router).await.unwrap()
