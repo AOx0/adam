0db30f72bc4d248da295dc9b839022ad1e3e294e
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-09-17 21:09:45 -0600

    feat(adam-front): :rocket: add first react template

--- justfile
+++
@@ -1,5 +0,0 @@
-run-firewall iface='lo':
-    RUST_LOG=info cargo xtask run firewall --release -- -i {{iface}}
-
-run-controller:
-    cargo build --release && sudo ./target/release/controller
---
+++ service-monitor/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>¡Hola, bienvenido a mi aplicación React!</h1>
+        <p>
+          Este es un proyecto de ejemplo en React.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
