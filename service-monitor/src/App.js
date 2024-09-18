22e42c78f7a9b468ecc43320efb43298f95f74d1
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-09-17 21:22:39 -0600

    feat(adam-front): :sparkles: add first firewall home page

--- service-monitor/src/App.js
+++ service-monitor/src/App.js
@@ -1,15 +1,47 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>¡Hola, bienvenido a mi aplicación React!</h1>
-        <p>
-          Este es un proyecto de ejemplo en React.
-        </p>
-      </header>
+    <Router>
+      <div className="App">
+        <nav className="App-navbar">
+          <h1>ADAM Monitor</h1>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/firewall" element={<Page1 />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <header className="App-header">
+      <div className="grid-container">
+        <Link to="/firewall" className="grid-item">Firewall</Link>
+        <div className="grid-item">2</div>
+        <div className="grid-item">3</div>
+        <div className="grid-item">4</div>
+        <div className="grid-item">5</div>
+        <div className="grid-item">6</div>
+        <div className="grid-item">7</div>
+        <div className="grid-item">8</div>
+        <div className="grid-item">9</div>
+      </div>
+    </header>
+  );
+}
+
+function Page1() {
+  return (
+    <div className="page">
+      <h2>Firewall</h2>
+      <p>Firewall Desc</p>
+      <Link to="/">Back to Home</Link>
     </div>
   );
 }
