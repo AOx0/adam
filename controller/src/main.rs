ddfe7bc3fe99f02acc68843928e8b330dee3012a
Alejandro Osornio <aoxo.contact@gmail.com>
2024-11-13 19:42:30 -0600

    controller: Take binding address as arg

--- controller/src/main.rs
+++ controller/src/main.rs
@@ -1,5 +1,11 @@
+use std::{
+    net::{IpAddr, SocketAddr},
+    str::FromStr,
+};
+
 pub use axum::{extract::State, routing::post, Router};
 use axum::{middleware::Next, response::Response};
+use clap::Parser;
 use deadpool::managed::Pool;
 use tokio::net::TcpListener;
 use tower_http::cors::{Any, CorsLayer};
@@ -30,8 +36,23 @@ pub async fn insert_headers(req: axum::extract::Request, next: Next) -> Response
     response
 }
 
+#[derive(Debug, Parser)]
+struct Args {
+    #[clap(long, short, alias = "addr")]
+    address: Option<IpAddr>,
+    #[clap(long, short)]
+    port: Option<u16>,
+}
+
 #[tokio::main]
 async fn main() {
+    let Args { address, port } = Args::parse();
+    let address = address.unwrap_or(IpAddr::from_str("::").expect("Should not fail"));
+    let port = port.unwrap_or(9988);
+
+    let socket = SocketAddr::new(address, port);
+    println!("Binding to {socket:?}");
+
     let state = AppState::new();
 
     let cors = CorsLayer::new()
@@ -46,6 +67,6 @@ async fn main() {
         .layer(cors)
         .with_state(state);
 
-    let listener = TcpListener::bind("[::]:9988").await.unwrap();
+    let listener = TcpListener::bind(socket).await.unwrap();
     axum::serve(listener, router).await.unwrap()
 }
